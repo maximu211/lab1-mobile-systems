276b74f1e9af337bcdd7b96ebf3a65ebbea6fa94
maximu211 <63352037+maximu211@users.noreply.github.com>
2024-02-22 23:50:14 +0200

    Add files via upload

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var num1, num2: Int?
+var result: Float?
+var operation: String?
+
+operation = "/"
+num1 = 10
+num2 = 2
+
+func minus(_ num1: Int, _ num2: Int) -> Int{
+    return num1 - num2
+}
+
+func plus(_ num1: Int,_ num2: Int) -> Int{
+    return num1 + num2
+}
+
+func multiply(_ num1: Int, _ num2: Int) -> Int{
+    return num1 * num2
+}
+
+func division(_ num1: Int, _ num2: Int) -> Int?{
+    guard num2 != 0 else{
+        print("division by 0")
+        return nil
+    }
+        return num1 / num2
+}
+
+if let num1 = num1, let num2 = num2{
+    switch operation {
+    case "/"?:
+        print(division(num1,num2))
+    case "+"?:
+        print(plus(num1, num2))
+    case "*"?:
+        print(multiply(num1, num2))
+    default:
+        print("Input valid operator")
+    }
+}
